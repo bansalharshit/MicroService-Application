4d9d9b223056b65401b42590a70493509a3bd128
Harshit Bansal <harshitbansal394@gmail.com>
2023-02-06 11:39:40 +0530

    add ServiceRegistry

--- Microservice-HotelService/src/main/java/com/masai/hotel/MicroserviceHotelServiceApplication.java
+++ Microservice-HotelService/src/main/java/com/masai/hotel/MicroserviceHotelServiceApplication.java
@@ -2,8 +2,10 @@ package com.masai.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceHotelServiceApplication {
 
 	public static void main(String[] args) {
